5b27eafb8979fc9de3a6e5ef6b56be87a128dbce
13621151569@yeah.net <13621151569@yeah.net>
2018-12-23 12:26:29 +0800

    mss-gateway

--- mss-gateway/node-1.bat
+++ mss-gateway/node-1.bat
@@ -1,1 +1,4 @@
-java -jar .\target\Micro-Service-Skeleton-Gateway-0.0.1-SNAPSHOT.jar
+set workspace_dir=D:\work-dir\spring-cloud-all-in-one-sample\Micro-Service-Skeleton\mss-gateway
+cd %workspace_dir%
+call mvn install -Dmaven.test.skip=true
+java -jar .\target\mss-gateway-2.0.0.jar
